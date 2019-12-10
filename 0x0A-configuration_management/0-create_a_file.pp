eda53720b8b47cef86398313c906dca5a663511f
Aalaa Satti <ala.k.satti@gmail.com>
2019-12-10 01:29:44 +0000

    puupet-lint compliant

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # create file holberton
 file { 'holberton':
-  ensure	=> file,
-  content	=> 'I love Puppet',
-  mode		=> '0744',
-  owner		=> 'www-data',
-  group		=> 'www-data',
-  path		=> '/tmp/holberton'
+  ensure  => file,
+  content => 'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  path    => '/tmp/holberton'
 }
