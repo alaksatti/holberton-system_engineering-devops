e83394d40bd912fa407b9ebf449ae197c6a85aa0
Aalaa Satti <ala.k.satti@gmail.com>
2019-12-10 01:07:53 +0000

    initial commit

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# create file holberton
+file { '/tmp/holberton':
+  ensure	=> file,
+  content	=> 'I love Puppet',
+  mode	        => '0744',
+  owner         => 'www-data',
+  group         => 'www-data',
+  path          => '/tmp/holberton'
+}
