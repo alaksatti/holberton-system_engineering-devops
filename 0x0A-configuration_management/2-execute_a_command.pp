b26ba5fb6e0400bd790161c095018fd60206a6df
Aalaa Satti <ala.k.satti@gmail.com>
2019-12-10 01:59:28 +0000

    remove provider

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,5 @@
 # kill a process using pkil
 exec { 'killmenow':
   command  => 'pkill killmenow',
-  path     => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
-  provider => 'shell'
+  path     => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ]
 }
