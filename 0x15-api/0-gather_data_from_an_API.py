a9428509efec1e3102626de808ff4f211e863446
alaksatti <ala.k.satti@gmail.com>
2020-02-04 05:26:23 +0000

    initial commit

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+''' using a specific REST API for a given employee return his todo list '''
+
+def employee_info(uid):
+    ''' retrieve employee info '''
+    employee = get("https://jsonplaceholder.typicode.com/users/" + uid).json()
+
+    if employee:
+        tasks = get('https://jsonplaceholder.typicode.com/todos?userId='
+                    + uid).json()
+        task_total = len(tasks)
+        employee_id = employee.get('id')
+        employee_name = employee.get('name')
+        completed_tasks = []
+
+        for task in tasks:
+            if task.get('completed'):
+                completed_tasks.append('\t ' + task.get('title'))
+        total_completed = len(completed_tasks)
+        print("Employee {} is done with tasks({}/{}):".format(employee_name,
+                                                              total_completed,
+                                                              task_total))
+        print('\n'.join(completed_tasks))
+
+
+
+if __name__ == '__main__':
+    from requests import get
+    from sys import argv
+
+
+    if len(argv) == 2 and argv[1].isdigit():
+        employee_info(argv[1])
