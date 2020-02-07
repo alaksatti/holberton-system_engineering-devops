584b3d6add075e8da9a44d0c48705d8a3fbec87c
alaksatti <ala.k.satti@gmail.com>
2020-02-07 07:40:00 +0000

    initial commit

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+''' Queries the Reddit API '''
+from requests import get
+url = 'https://www.reddit.com'
+headers = {'User-Agent': 'alaksatti'}
+
+
+def recurse(sr_input, hot_list=[], next_page=None):
+    '''recursive function to return list of hot articles'''
+    subreddit_page = url + '/r/' + sr_input + '/hot.json'
+
+    if next_page:
+        subreddit_page = subreddit_page + '?after=' + next_page
+
+    subreddit = get(subreddit_page, headers=headers, allow_redirects=False)
+    if subreddit.status_code != 200:
+        return None
+    list_elements = subreddit.json().get('data').get('children')
+    next_page = subreddit.json().get('data').get('after')
+
+    for e in list_elements:
+         hot_list.append(e.get('data').get('title'))
+    if next_page:
+        return recurse(sr_input, hot_list, next_page)
+    return hot_list
