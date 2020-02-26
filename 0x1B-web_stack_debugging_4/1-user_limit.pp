e1e083a20fcfc236ce2009ee0e927330c87500d8
alaksatti <ala.k.satti@gmail.com>
2020-02-26 04:09:05 +0000

    initial commit

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,9 @@
+# set user limits
+exec { 'hard limit':
+  command => 'sed -i "s/holberton hard nofile 5/holberton hard nofile 1000/" /etc/security/limits.conf',
+  path    => '/bin',
+}
+exec { 'soft limit':
+  command => 'sed -i "s/holberton soft nofile 4/holberton hard nofile 1000/" /etc/security/limits.conf',
+  path    => '/bin',
+}
