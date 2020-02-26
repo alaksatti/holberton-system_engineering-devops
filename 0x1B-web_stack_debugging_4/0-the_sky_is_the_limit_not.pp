ee2592101337d7610091dd47d02f73ffdaa2a6ea
alaksatti <ala.k.satti@gmail.com>
2020-02-26 02:57:58 +0000

    initial commit

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,9 @@
+# chnage upper limit
+exec { 'upper limit':
+  command => 'sed -i s/15/1000/ /etc/default/nginx',
+  path    => '/bin',
+}
+service { 'nginx':
+    ensure    => running,
+    subscribe => Exec['upper limit'],
+}
